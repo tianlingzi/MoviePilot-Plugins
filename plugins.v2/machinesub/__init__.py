d481845beb207aee4498adc14e0d4ebaba057eb2
tianlingzi <2814782171@qq.com>
2026-09-20 22:49:21 +0800

    兼容moviepolit V3

--- plugins.v2/machinesub/__init__.py
+++ plugins.v2/machinesub/__init__.py
@@ -68,7 +68,7 @@ class MachineSub(_PluginBase):
     # 主题色
     plugin_color = "#87CEEB"
     # 插件版本
-    plugin_version = "1.5"
+    plugin_version = "1.6"
     # 插件作者
     plugin_author = "tianlingzi"
     # 作者主页
